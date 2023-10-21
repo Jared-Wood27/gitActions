76e78762a8b6c7009953ee9ce5e1576841b3c11b
Jared <jayrod.wood@gmail.com>
2023-10-21 01:26:17 -0600

    mainImplementation4

--- github-calculator/src/main/java/Main.java
+++ github-calculator/src/main/java/Main.java
@@ -4,54 +4,62 @@ public class Main {
 
   public static void main(String[] args) {
     //Main Test1
+    boolean keepGoing = true;
     Calculator calculator = new Calculator();
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Enter a command (add/subtract/multiply/divide/fibonacci/intToBinary/done):");
+    System.out.println("Enter a command (add/subtract/multiply/divide/fibonacci/binary/done):");
     String command = scanner.next();
 
-    while(command != "done") {
+    while(keepGoing) {
       switch (command) {
         case "add":
           int addA=scanner.nextInt();
           int addB=scanner.nextInt();
           int addResult=calculator.add(addA, addB);
-          System.out.println("Result: " + addResult);
+          System.out.println(addResult);
           break;
         case "subtract":
           int subA=scanner.nextInt();
           int subB=scanner.nextInt();
           int subResult=calculator.subtract(subA, subB);
-          System.out.println("Result: " + subResult);
+          System.out.println(subResult);
           break;
         case "multiply":
           int mulA=scanner.nextInt();
           int mulB=scanner.nextInt();
           int mulResult=calculator.multiply(mulA, mulB);
-          System.out.println("Result: " + mulResult);
+          System.out.println(mulResult);
           break;
         case "divide":
           int divA=scanner.nextInt();
           int divB=scanner.nextInt();
           int divResult=calculator.divide(divA, divB);
-          System.out.println("Result: " + divResult);
+          System.out.println(divResult);
           break;
         case "fibonacci":
           int fibN=scanner.nextInt();
           int fibResult=calculator.fibonacciNumberFinder(fibN);
-          System.out.println("Result: " + fibResult);
+          System.out.println(fibResult);
           break;
-        case "intToBinary":
+        case "binary":
           int binaryN=scanner.nextInt();
           String binaryResult=calculator.intToBinaryNumber(binaryN);
-          System.out.println("Result: " + binaryResult);
+          System.out.println(binaryResult);
+          break;
+        case "done":
+          System.out.println("Not even a thank you?");
+          keepGoing = false;
           break;
         default:
           System.out.println("Invalid command.");
       }
-      command = scanner.next();
+      if(keepGoing == true){
+        command = scanner.next();
+      }
     }
 
     scanner.close();
+    System.out.println("Goodbye!");
   }
 }
